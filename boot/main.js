b5bf3e0500c069aa9287345a0d9797068176dc07
Marx <52163001+3m1n3nc3@users.noreply.github.com>
2022-05-26 20:05:31 +0100

    Delete boot directory

--- boot/main.js
+++
@@ -1,8 +0,0 @@
-import { boot } from 'quasar/wrappers'
-import loadStore from "stores/loader";
-
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ app, router, store }) => {
-  app.config.globalProperties.$store = loadStore
-})
